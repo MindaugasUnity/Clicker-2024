4df153f77f9ff3e07dd8d906187da8afdd220871
Mindaugas Butkus <mindaugas.unity@gmail.com>
2024-07-03 19:33:35 +0300

    [Lesson: 2] Unity setup + what is method, algorithm, variables, variables types and scopes

---
+++ Assets/Scripts/EverythingScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class EverythingScript : MonoBehaviour
+{
+    // Method - Description for PC to do something
+    // Algorithm - Steps how to do something
+    
+    // Variables - stores data (Currency/Clicking power)
+    // Variables types - stores different type (whole number, number with decimal value, text, symbol, image, bool (0,1 | True or false))
+    
+    // Naming conventions - how to name variables
+        // camelCase - start with lower letter, everyNextWorldIsCapitalized
+        // PascalCase - start with Capital letter and everyNextWorldIsCapitalized
+        
+    // Scopes - defines area where methods and variables lasts/valid
+        // Defined by {}
+    
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
